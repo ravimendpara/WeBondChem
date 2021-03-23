e7a7da1ce2d3393b669e888a886cfe2091382adf
Remish Varsani <scspl.remish@gmail.com>
2021-03-23 17:52:12 +0530

    added registration form.-Remish

--- app/src/main/java/com/webond/chemicals/customer/CustomerRegistrationActivity.java
+++ app/src/main/java/com/webond/chemicals/customer/CustomerRegistrationActivity.java
@@ -1,16 +1,41 @@
 package com.webond.chemicals.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.webond.chemicals.R;
+import com.webond.chemicals.custom_class.TextViewRegularFont;
 
-public class CustomerRegistrationActivity extends AppCompatActivity {
+public class CustomerRegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AppCompatImageView imgBack;
+    private TextViewRegularFont tvHeaderTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_registration);
+        initView();
+    }
+
+    private void initView() {
+        imgBack = findViewById(R.id.imgBack);
+        imgBack.setOnClickListener(this);
+        tvHeaderTitle = findViewById(R.id.tvHeaderTitle);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.imgBack) {
+            onBackPressed();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
